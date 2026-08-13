d6d7989934e5403400ed1886f2162d14ed7b465d
CAO NGUYEN HOANG <1026cnh@gmail.com>
2026-08-13 21:18:41 +0700

    Add files via upload

---
+++ CTK48B_2411841_CaoNguyenHoang/BaiThucHanhBuoi01/Bai01/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bai01
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void txtTen_TextChanged(object sender, EventArgs e)
+        {
+            txtsaochep.Text = txtTen.Text;
+        }
+
+        private void XacNhan_Click(object sender, EventArgs e)
+        {
+            var nhaptenban = txtTen.Text;
+            MessageBox.Show($"Chao {nhaptenban} rất vui được gặp bạn");
+        }
+
+        private void saochep_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
