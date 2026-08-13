4ab82a5014fa8f4a06159fd3cb8369f38c69d5c8
CAO NGUYEN HOANG <1026cnh@gmail.com>
2026-08-14 00:12:33 +0700

    Add files via upload

---
+++ CTK48B_2411841_CaoNguyenHoang02/BaiThucHanhBuoi01/Bai2/MainForm.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace Bai2
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.rdbNu = new System.Windows.Forms.RadioButton();
+            this.rdbNam = new System.Windows.Forms.RadioButton();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.rdbMauXanh = new System.Windows.Forms.RadioButton();
+            this.rdDo = new System.Windows.Forms.RadioButton();
+            this.btnToMau = new System.Windows.Forms.Button();
+            this.txtHopMau = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rdbNu);
+            this.groupBox1.Controls.Add(this.rdbNam);
+            this.groupBox1.Location = new System.Drawing.Point(232, 105);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(299, 127);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "chọn giới tính";
+            // 
+            // rdbNu
+            // 
+            this.rdbNu.AutoSize = true;
+            this.rdbNu.Location = new System.Drawing.Point(21, 79);
+            this.rdbNu.Name = "rdbNu";
+            this.rdbNu.Size = new System.Drawing.Size(54, 24);
+            this.rdbNu.TabIndex = 2;
+            this.rdbNu.TabStop = true;
+            this.rdbNu.Text = "Nữ";
+            this.rdbNu.UseVisualStyleBackColor = true;
+            this.rdbNu.CheckedChanged += new System.EventHandler(this.rdbNu_CheckedChanged);
+            // 
+            // rdbNam
+            // 
+            this.rdbNam.AutoSize = true;
+            this.rdbNam.Checked = true;
+            this.rdbNam.Location = new System.Drawing.Point(21, 34);
+            this.rdbNam.Name = "rdbNam";
+            this.rdbNam.Size = new System.Drawing.Size(67, 24);
+            this.rdbNam.TabIndex = 1;
+            this.rdbNam.TabStop = true;
+            this.rdbNam.Text = "Nam";
+            this.rdbNam.UseVisualStyleBackColor = true;
+            this.rdbNam.CheckedChanged += new System.EventHandler(this.rdbNam_CheckedChanged);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnToMau);
+            this.groupBox2.Controls.Add(this.rdbMauXanh);
+            this.groupBox2.Controls.Add(this.rdDo);
+            this.groupBox2.Location = new System.Drawing.Point(232, 252);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(299, 127);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "chọn màu";
+            // 
+            // rdbMauXanh
+            // 
+            this.rdbMauXanh.AutoSize = true;
+            this.rdbMauXanh.Location = new System.Drawing.Point(21, 79);
+            this.rdbMauXanh.Name = "rdbMauXanh";
+            this.rdbMauXanh.Size = new System.Drawing.Size(107, 24);
+            this.rdbMauXanh.TabIndex = 2;
+            this.rdbMauXanh.TabStop = true;
+            this.rdbMauXanh.Text = "Màu Xanh";
+            this.rdbMauXanh.UseVisualStyleBackColor = true;
+            // 
+            // rdDo
+            // 
+            this.rdDo.AutoSize = true;
+            this.rdDo.Checked = true;
+            this.rdDo.Location = new System.Drawing.Point(21, 34);
+            this.rdDo.Name = "rdDo";
+            this.rdDo.Size = new System.Drawing.Size(87, 24);
+            this.rdDo.TabIndex = 1;
+            this.rdDo.TabStop = true;
+            this.rdDo.Text = "Màu đỏ";
+            this.rdDo.UseVisualStyleBackColor = true;
+            // 
+            // btnToMau
+            // 
+            this.btnToMau.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.btnToMau.Location = new System.Drawing.Point(168, 59);
+            this.btnToMau.Name = "btnToMau";
+            this.btnToMau.Size = new System.Drawing.Size(106, 44);
+            this.btnToMau.TabIndex = 3;
+            this.btnToMau.Text = "Tô màu";
+            this.btnToMau.UseVisualStyleBackColor = false;
+            this.btnToMau.Click += new System.EventHandler(this.btnToMau_Click);
+            // 
+            // txtHopMau
+            // 
+            this.txtHopMau.Location = new System.Drawing.Point(557, 264);
+            this.txtHopMau.Multiline = true;
+            this.txtHopMau.Name = "txtHopMau";
+            this.txtHopMau.Size = new System.Drawing.Size(178, 115);
+            this.txtHopMau.TabIndex = 2;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtHopMau);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "MainForm";
+            this.Text = "Bai Tap 2";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton rdbNam;
+        private System.Windows.Forms.RadioButton rdbNu;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RadioButton rdbMauXanh;
+        private System.Windows.Forms.RadioButton rdDo;
+        private System.Windows.Forms.Button btnToMau;
+        private System.Windows.Forms.TextBox txtHopMau;
+    }
+}
+
